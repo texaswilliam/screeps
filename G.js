36ac7ae33282e1ca81257821c9e38691cc5707bc
William Pyburn <texas.william@gmail.com>
2016-07-08 19:33:49 -0500

    G is a small convenience for the command line.

---
+++ G.js
@@ -0,0 +1,8 @@
+module.exports = {
+    get c() { return Game.creeps; },
+    get cs() { return Game.constructionSites; },
+    get f() { return Game.flags; },
+    get r() { return Game.rooms; },
+    get sp() { return Game.spawns; },
+    get st() { return Game.structures; }
+};
--- main.js
+++ main.js
@@ -1,1 +1,3 @@
+G = require('G');
+
 module.exports.loop = function() {};
