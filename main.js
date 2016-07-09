e0f278a4939f60eafec20b2c2898557eeb2a1d6a
William Pyburn <texas.william@gmail.com>
2016-07-09 18:19:54 -0500

    Forgot boilerplate Memory.creeps cleanup.

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let builder = require('builder');
 let controller_harv = require('controller_harv');
 
 module.exports.loop = function() {
+    for (let name in Memory.creeps) { if (!Game.creeps[name]) { delete Memory.creeps[name]; } }
+    
     builder.run();
     controller_harv.run();
 };
