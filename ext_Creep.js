0774d1d043925b896a7feb49a8e8272b06136d2c
William Pyburn <texas.william@gmail.com>
2016-07-12 18:23:20 -0500

    Creep.prototype.getBodyCost()

--- ext_Creep.js
+++ ext_Creep.js
@@ -1,18 +1,21 @@
 /** @module ext_Creep */
 
+let _ = require('lodash');
+
 /**
  * @param {string[]} body
  * @returns {number}
  */
-Creep.getBodyCost = function(body) { return _.sum(body, part => BODYPART_COST[part]); }
+Creep.getBodyCost = function(body) { return _.sum(body, part => BODYPART_COST[part]); };
+
+/** @returns {number} */
+Creep.prototype.getBodyCost = function() { return Creep.getBodyCost(this.body); };
 
 /**
  * @param {(RoomPosition|RoomObject)} target
  * @returns {number}
  */
-Creep.prototype.moveAwayFrom = function(target) {
-    return this.move((this.pos.getDirectionTo(target) + 3) % 8 + 1);
-};
+Creep.prototype.moveAwayFrom = function(target) { return this.move((this.pos.getDirectionTo(target) + 3) % 8 + 1); };
 
 /**
  * @param {(RoomPosition|RoomObject)} target
