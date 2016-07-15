abbc31cdaa96acd379ce746871d14d5f438e5625
William Pyburn <texas.william@gmail.com>
2016-07-15 02:49:27 -0500

    Only repair roads that have < 4500 hits.

--- should.js
+++ should.js
@@ -9,7 +9,7 @@ module.exports.creep = {};
  */
 module.exports.creep.repair = function(source, target) {
     let repPower = source.body.filter(p => p.type === WORK).length * REPAIR_POWER;
-    let hitsLimit = target instanceof StructureRoad ? 10000 : 20000;
+    let hitsLimit = target instanceof StructureRoad ? 4500 : 20000;
     return (target.hits <= target.hitsMax - repPower || target.hits < target.hitsMax / 2) && target.hits < hitsLimit;
 };
 
@@ -21,6 +21,6 @@ module.exports.tower = {};
  */
 module.exports.tower.repair = function(target) {
     let repPower = TOWER_POWER_REPAIR;
-    let hitsLimit = target instanceof StructureRoad ? 10000 : 20000;
+    let hitsLimit = target instanceof StructureRoad ? 4500 : 20000;
     return (target.hits <= target.hitsMax - repPower || target.hits < target.hitsMax / 2) && target.hits < hitsLimit;
 };
