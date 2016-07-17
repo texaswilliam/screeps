dbb0833834d2ecdc8448a33726fcaaa7e87bc5b9
William Pyburn <texas.william@gmail.com>
2016-07-17 16:27:24 -0500

    Multiroom fix for builder.

--- builder.js
+++ builder.js
@@ -19,15 +19,17 @@ module.exports.ensureMaxStructureType = function(roomName, jsType, structureType
         let diff = max - num;
         let spawn = room.find(FIND_MY_SPAWNS)[0];
         
-        for (let radius = 2; diff > 0; ++radius) {
-            for (let i = -radius; diff > 0 && i <= radius; i += 2) {
-                for (let j = -radius; diff > 0 && j <= radius; j += 2) {
-                    if (i >= -1 && i <= 1 && j >= -1 && j <= 1) { continue; }
-                    let x = spawn.pos.x + i;
-                    let y = spawn.pos.y + j;
-                    if (x < 0 || x > 49 || y < 0 || y > 49) { continue; }
-                    let status = room.createConstructionSite(x, y, structureType);
-                    if (status == OK) { --diff; }
+        if (spawn) {
+            for (let radius = 2; diff > 0; ++radius) {
+                for (let i = -radius; diff > 0 && i <= radius; i += 2) {
+                    for (let j = -radius; diff > 0 && j <= radius; j += 2) {
+                        if (i >= -1 && i <= 1 && j >= -1 && j <= 1) { continue; }
+                        let x = spawn.pos.x + i;
+                        let y = spawn.pos.y + j;
+                        if (x < 0 || x > 49 || y < 0 || y > 49) { continue; }
+                        let status = room.createConstructionSite(x, y, structureType);
+                        if (status == OK) { --diff; }
+                    }
                 }
             }
         }
